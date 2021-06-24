28ce115dcadaa6cba38e5e66dd0ef9e5aefc53ee
Cristian Sidabutar <86230103+cristiansidabutar@users.noreply.github.com>
2021-06-24 09:39:32 +0700

    Program Luas Lingkaran

---
+++ Kalkulator Luas Lingkaran/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Kalkulator_Luas_Lingkaran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.find = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.jari_jari = new System.Windows.Forms.TextBox();
+            this.hasil = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pilihan = new System.Windows.Forms.ComboBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.finish = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // find
+            // 
+            this.find.BackColor = System.Drawing.Color.DarkSalmon;
+            this.find.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.find.Location = new System.Drawing.Point(38, 206);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(89, 34);
+            this.find.TabIndex = 0;
+            this.find.Text = "Find";
+            this.find.UseVisualStyleBackColor = false;
+            this.find.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Green;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 52);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Area and Circumference\r\nCircle Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(38, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Circle Radius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result";
+            // 
+            // jari_jari
+            // 
+            this.jari_jari.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jari_jari.Location = new System.Drawing.Point(214, 104);
+            this.jari_jari.Name = "jari_jari";
+            this.jari_jari.Size = new System.Drawing.Size(108, 22);
+            this.jari_jari.TabIndex = 6;
+            // 
+            // hasil
+            // 
+            this.hasil.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hasil.Location = new System.Drawing.Point(214, 167);
+            this.hasil.Name = "hasil";
+            this.hasil.Size = new System.Drawing.Size(108, 22);
+            this.hasil.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 27);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Selection";
+            // 
+            // pilihan
+            // 
+            this.pilihan.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pilihan.FormattingEnabled = true;
+            this.pilihan.Items.AddRange(new object[] {
+            "Area of Circle",
+            "Circumference"});
+            this.pilihan.Location = new System.Drawing.Point(214, 135);
+            this.pilihan.Name = "pilihan";
+            this.pilihan.Size = new System.Drawing.Size(108, 24);
+            this.pilihan.TabIndex = 10;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.DarkSalmon;
+            this.clear.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(138, 206);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(89, 34);
+            this.clear.TabIndex = 11;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // finish
+            // 
+            this.finish.BackColor = System.Drawing.Color.DarkSalmon;
+            this.finish.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finish.Location = new System.Drawing.Point(233, 206);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(89, 34);
+            this.finish.TabIndex = 12;
+            this.finish.Text = "Finish";
+            this.finish.UseVisualStyleBackColor = false;
+            this.finish.Click += new System.EventHandler(this.finish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(361, 259);
+            this.Controls.Add(this.finish);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.pilihan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hasil);
+            this.Controls.Add(this.jari_jari);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.find);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox jari_jari;
+        private System.Windows.Forms.TextBox hasil;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox pilihan;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button finish;
+    }
+}
+
---
+++ Kalkulator Luas Lingkaran/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_Luas_Lingkaran
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(jari_jari.Text))
+            {
+                MessageBox.Show("Please Enter The Value of The Radius!");   
+            }
+            else if(pilihan.SelectedIndex == 0)
+            {
+                double a = 3.14, b, c;
+                b = double.Parse(this.jari_jari.Text);
+                c = a * b * b;
+                this.hasil.Text = c.ToString();
+            }
+            else if(pilihan.SelectedIndex == 1)
+            {
+                double a = 3.14, b, c;
+                b = double.Parse(this.jari_jari.Text);
+                c = 2 * a * b;
+                this.hasil.Text = c.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            jari_jari.Clear();
+            hasil.Clear();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            jari_jari.Clear();
+            hasil.Clear();
+        }
+
+        private void finish_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
